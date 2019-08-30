565a8f07feb047984686afbb4e7c7fc2531786e0
kanagasabapathy <50829325+KanagasabapathyPalanisamy@users.noreply.github.com>
2019-08-30 06:07:37 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -7,26 +7,26 @@ import Card from './Card';
     return(
       <div className="bbb">
 
-<Card name="Rohit Sharma" age="ff" TsNo="45"/>
-<Card name="Rahul" age="22"TsNo="21"/>
-<Card name="virat Kholi" age="11"TsNo="18"/>
-<Card name="H Pandiya" age="aa"TsNo="333"/>
-<Card name="Pant" age="kumar"TsNo="05"/>
-<Card name="Dhoni" age="dinesh"TsNo="07"/>
-<Card name="Karthik" age="bala"TsNo="12"/>
-<Card name="Bhuvi" age="27"TsNo="15"/>
-<Card name="Mohammed Shami" age="2"TsNo="23"/>
-<Card name="kuldeep" age="29"TsNo="43"/>
-<Card name="Bumrah" age="kags"TsNo="65"/>
-<Card name="kedhar" age="18"TsNo="75"/>
-<Card name="Dhawan" age="17"TsNo="48"/>
-<Card name="Chahal" age="ramu"TsNo="49"/>
-<Card name="Jadeja" age="15"TsNo="11"/>
-<Card name="Mumbai Boys" age="8"TsNo="111"/>
-<Card name="Chennai Boys" age="7"TsNo="415"/>
-<Card name="SunRises Boys" age="ramu"TsNo="675"/>
-<Card name="Delhi Boys" age="5"TsNo="34"/>
-<Card name="Banglore Boys" age="95"TsNo="934"/>
+<Card name="Rohit Sharma" age="ff" TsNo="45" Role="Bat/Spin"/>
+<Card name="Rahul" age="22"TsNo="21" Role="Bat/Wk"/>
+<Card name="virat Kholi" age="11"TsNo="18" Role="Bat/fast"/>
+<Card name="H Pandiya" age="aa"TsNo="333" Role="Bat/fast"/>
+<Card name="Pant" age="kumar"TsNo="05" Role="Bat/Wk"/>
+<Card name="Dhoni" age="dinesh"TsNo="07" Role="Bat/Wk"/>
+<Card name="Karthik" age="bala"TsNo="12" Role="Bat/Wk"/>
+<Card name="Bhuvi" age="27"TsNo="15" Role="Bat/Fast"/>
+<Card name="Mohammed Shami" age="2"TsNo="23" Role="Bat/Fast"/>
+<Card name="kuldeep" age="29"TsNo="43" Role="Bat/Spin"/>
+<Card name="Bumrah" age="kags"TsNo="65" Role="Fast"/>
+<Card name="kedhar" age="18"TsNo="75" Role="Bat/Wk"/>
+<Card name="Dhawan" age="17"TsNo="48" Role="Bat"/>
+<Card name="Chahal" age="ramu"TsNo="49" Role="Spin"/>
+<Card name="Jadeja" age="15"TsNo="11" Role="Bat/Spin"/>
+<Card name="Mumbai Boys" age="8"TsNo="111" Role="Bat/Spin"/>
+<Card name="Chennai Boys" age="7"TsNo="415" Role="Bat/Spin"/>
+<Card name="SunRises Boys" age="ramu"TsNo="675" Role="Bat/Spin"/>
+<Card name="Delhi Boys" age="5"TsNo="34" Role="Bat/Spin"/>
+<Card name="Banglore Boys" age="95"TsNo="934" Role="Bat/Spin"/>
       </div>
   )
   }
--- src/Card.js
+++ src/Card.js
@@ -5,6 +5,7 @@ function Card(props){
 <img src={`https://robohash.org/${props.age}?set=set5`}width="210"/>
             <p>{props.name}</p>
            <p>{props.TsNo}</p>
+           <p>{props.Role}</p>
     </div>
     )
 
